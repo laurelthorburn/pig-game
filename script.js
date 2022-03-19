47e48e76661758a6af8edbc5fa818802f3f44e12
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-19 16:07:27 -0600

    Setting up basic dice roll function to display correct img

--- script.js
+++ script.js
@@ -5,10 +5,30 @@ const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 
-console.log(diceEl)
+const newBtn = document.querySelector('.btn--new');
+const rollBtn = document.querySelector('.btn--roll');
+const holdBtn = document.querySelector('.btn--hold');
+
 //initial scores
 score0El.textContent = 0;
 score1El.textContent = 0;
 
 //create hidden class on dice
 diceEl.classList.add('hidden');
+
+// ROlling dice functionality
+rollBtn.addEventListener('click', function(){
+    //1. generate a random #/dice roll
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
+    console.log(diceRoll)
+    //2. display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${diceRoll}.png`;
+    //3. check if rolled 1 -- yes = next player
+    if(diceRoll !== 1){
+        console.log("yay");
+        score0El = score0El + diceRoll;
+    } else {
+        console.log("fudge");
+    }
+})
