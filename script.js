4db33cf6d1ba463e65ea907cef98a4f00a8dd95b
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-19 15:46:29 -0600

    Hid dice

--- script.js
+++ script.js
@@ -3,6 +3,12 @@
 // Selecting elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
 
+console.log(diceEl)
+//initial scores
 score0El.textContent = 0;
 score1El.textContent = 0;
+
+//create hidden class on dice
+diceEl.classList.add('hidden');
